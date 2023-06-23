02a44156a44a9e55c87e84bc1fb86ee3e7a48708
Ga7briel27 <110438236+Ga7briel27@users.noreply.github.com>
2023-06-22 22:46:18 -0400

    Add files via upload

---
+++ week1Pythonprogramming (2)-3.py
@@ -0,0 +1,12 @@
+"""Week 1 Hands on
+Author: Letrell Hodge
+Date: 5-26-2023
+Program: triangle"""
+side1=int(input("Enter the length of side 1 of the triangle: 10"))
+side2=int(input("Enter the length of side 2 of the triangle: 11")) 
+side3=int (input("Enter the length of side 3 of the triangle: 12"))
+if side1==side2 and side2==side3:
+    'else'
+print("The triangle is an equilateral!") 
+print("The triangle is NOT an equilateral!")
+
